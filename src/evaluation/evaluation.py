67462ab362d42a7574d052c19bd1269c9d37882a
Andreas Skov Millarch <andreasmillarch@gmail.com>
2025-03-12 20:06:56 +0100

    added the full model training script and evaluation functions

---
+++ src/evaluation/evaluation.py
@@ -0,0 +1,269 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from azureml.core import Experiment, Run, Workspace
+import mlflow
+from mlflow import MlflowClient
+from src.training.utils import get_threshold
+import numpy as np 
+import ast
+import os
+
+from src.visualize import plot_evaluation, plot_box_kde, plot_loss, plot_fold_evaluation, create_calibration_plot, evaluate_detection_rate, plot_hist_kde
+from sklearn.metrics import auc, roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
+
+from scipy import stats
+from sklearn.metrics import roc_auc_score
+
+def delong_roc_variance(ground_truth, predictions):
+    """
+    Computes ROC AUC variance using DeLong's method.
+    
+    Args:
+    ground_truth (array-like): True binary labels
+    predictions (array-like): Target scores (probability estimates)
+    
+    Returns:
+    float: Variance of ROC AUC
+    """
+    order = np.argsort(predictions)
+    ground_truth = ground_truth[order]
+    predictions = predictions[order]
+    
+    n_pos = np.sum(ground_truth)
+    n_neg = len(ground_truth) - n_pos
+    
+    pos_ranks = np.where(ground_truth == 1)[0] + 1
+    neg_ranks = np.where(ground_truth == 0)[0] + 1
+    
+    auc = (np.sum(pos_ranks) - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg)
+    
+    v01 = (auc / (2 - auc) - auc ** 2) / (n_neg)
+    v10 = (2 * auc ** 2 / (1 + auc) - auc ** 2) / (n_pos)
+    
+    return v01 + v10
+
+def calculate_roc_auc_ci(y_true, y_pred, alpha=0.95):
+    """
+    Calculate ROC AUC score and its confidence interval using DeLong method.
+    
+    Args:
+    y_true (array-like): True binary labels
+    y_pred (array-like): Target scores (probability estimates)
+    alpha (float): Confidence level (default: 0.95 for 95% CI)
+    
+    Returns:
+    tuple: (AUC score, lower bound of CI, upper bound of CI)
+    """
+    auc = roc_auc_score(y_true, y_pred)
+    auc_var = delong_roc_variance(y_true, y_pred)
+    
+    auc_std = np.sqrt(auc_var)
+    lower_upper_q = np.abs(np.array([0, 1]) - (1 - alpha) / 2)
+    ci = stats.norm.ppf(lower_upper_q, loc=auc, scale=auc_std)
+    ci[ci > 1] = 1
+    ci[ci < 0] = 0
+    return auc, ci[0], ci[1]
+
+
+def calculate_average_precision_ci(y_true, y_pred, alpha=0.95, n_bootstraps=1000):
+    """
+    Calculate average precision score and its confidence interval using bootstrapping.
+    
+    Args:
+    y_true (array-like): True binary labels
+    y_pred (array-like): Target scores (probability estimates)
+    alpha (float): Confidence level (default: 0.95 for 95% CI)
+    n_bootstraps (int): Number of bootstrap samples
+    
+    Returns:
+    tuple: (Average precision score, lower bound of CI, upper bound of CI)
+    """
+    ap = average_precision_score(y_true, y_pred)
+    
+    bootstrapped_scores = []
+    rng = np.random.RandomState(42)
+    for i in range(n_bootstraps):
+        indices = rng.randint(0, len(y_true), len(y_true))
+        if len(np.unique(y_true[indices])) < 2:
+            continue
+        score = average_precision_score(y_true[indices], y_pred[indices])
+        bootstrapped_scores.append(score)
+    
+    sorted_scores = np.array(bootstrapped_scores)
+    sorted_scores.sort()
+    
+    ci_lower = sorted_scores[int((1.0-alpha)/2 * len(sorted_scores))]
+    ci_upper = sorted_scores[int((1.0+alpha)/2 * len(sorted_scores))]
+    
+    return ap, ci_lower, ci_upper
+
+def is_float_in_range(value, start, end, step=0.1):
+    return start <= round(float(value), 1) <= end and round(float(value) % step, 1) == 0
+
+def dl_file_from_run(experiment, run_id, filename, output_prefix="", output_path= 'models/study/eval/'):
+    run = Run(experiment, run_id)
+    # find model in run, download to temporary directory
+    file_path = ["./"+i for i in run.get_file_names() if filename in i]
+    file_names = list(map(lambda x: x.split('/')[-1], file_path))
+    if len(file_names) == 1:
+        file_name = file_names[0]
+        tmp_path = output_path+output_prefix+file_name
+        run.download_file(file_path[0], tmp_path)
+    elif len(file_names) == 1:
+        print("multiple files found")
+        for file_name, fp  in zip(file_names,file_path):
+            tmp_path = output_path+output_prefix+file_name
+            run.download_file(fp, tmp_path)  
+    else:
+        print("no file found")
+    
+def get_internal_validation_thresholds(beta=2):
+
+    experiment_name = 'NEJM'
+    output_prefix=""
+    print(f"str{output_prefix}")
+
+
+    # Get the Azure ML workspace
+    workspace = Workspace.from_config()
+
+    # Set the MLflow tracking URI to point to your Azure ML workspace
+    mlflow.set_tracking_uri(workspace.get_mlflow_tracking_uri())
+    workspace = Workspace.from_config()
+    experiment = Experiment(workspace=workspace, name=experiment_name)
+    runs = experiment.get_runs()
+
+    keep_run_ids = []
+    for idx, run in enumerate(runs):
+        r = mlflow.get_run(run.id)
+       # print(r)
+        if r.data.params["model"]:
+            model_params = ast.literal_eval(r.data.params["model"])
+            if (is_float_in_range(model_params.get('res_dropout', 0), 0.2, 0.3) and 
+                is_float_in_range(model_params.get('fc_dropout', 0), 0.7, 0.8) and
+                model_params.get('n_layers') == 8 and
+                model_params.get('n_heads') == 8 and
+                model_params.get('d_model') == 64):
+                    keep_run_ids.append(run.id)
+                    print(f"kept run.id {run.id}")
+        
+    runs = experiment.get_runs()
+    for idx,run in enumerate(runs):
+        if run.id in keep_run_ids:
+            for child in run.get_children():
+                dl_file_from_run(experiment, child.id, "pred_df",output_prefix=f"preds/run_{idx}_")
+
+    folder_path = 'models/study/eval/preds'
+    thresholds= []
+    for filename in os.listdir(folder_path):
+        if filename.startswith('run'):
+            file_path = os.path.join(folder_path, filename)
+            df = pd.read_csv(file_path, index_col=0)
+            threshold = get_threshold(df["target"], df["prediction"], method="Fbeta-score", beta=beta)
+            thresholds.append(threshold)
+    return thresholds
+
+
+def calculate_net_benefit(y_true, y_pred, threshold):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    
+    # Ensure y_pred is probability (between 0 and 1)
+    if y_pred.ndim > 1:
+        y_pred = y_pred[:, 1]
+    y_pred = np.clip(y_pred, 0, 1)
+    
+    tp = np.sum((y_pred >= threshold) & (y_true == 1))
+    fp = np.sum((y_pred >= threshold) & (y_true == 0))
+    n = len(y_true)
+    
+    # Avoid division by zero
+    if threshold == 1:
+        return 0
+    return (tp / n) - (fp / n) * (threshold / (1 - threshold))
+
+def calculate_always_act(y_true, threshold):
+    prevalence = np.mean(y_true)
+    # Avoid division by zero
+    if threshold == 1:
+        return 0
+    return prevalence - (1 - prevalence) * (threshold / (1 - threshold))
+
+def plot_net_benefit(y_true, y_pred):
+    thresholds = np.linspace(0, 1, 100)
+
+    net_benefits = [calculate_net_benefit(y_true, y_pred, t) for t in thresholds]
+    always_act = [calculate_always_act(y_true, t) for t in thresholds]
+    never_act = [0] * len(thresholds)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(thresholds, net_benefits, label='Model')
+    plt.plot(thresholds, always_act, label='Always Act')
+    plt.plot(thresholds, never_act, label='Never Act')
+    
+    plt.xlabel('Threshold')
+    plt.ylabel('Net Benefit')
+    plt.title('Net Benefit Curve')
+    plt.legend()
+    plt.grid(True)
+
+    plt.ylim(-0.05, max(max(net_benefits), max(always_act)) + 0.05)
+    
+    plt.show()
+
+
+def evaluate(holdout_mixed_dls, learn , threshold):
+    # Get predictions on validation set
+    preds, target = learn.get_preds(dl=holdout_mixed_dls.train)
+    preds = preds[:,1].cpu().numpy()
+    target = target.cpu().numpy()
+
+    fig = plot_evaluation(preds, target,'90-day mortality')
+
+    # Calculate ROC curve and AUC
+    fpr, tpr, _ = roc_curve(target, preds)
+    roc_auc = auc(fpr, tpr)
+
+    
+    # Calculate PR curve and AP
+    precision, recall, _ = precision_recall_curve(target, preds)
+
+    ap = average_precision_score(target, preds)
+
+    # Calibration plot
+    fig = create_calibration_plot(target, preds, n_bins=4)
+    
+    # KDE plot
+    preds_df = pd.DataFrame()
+    preds_df['preds'] = preds.astype(float)
+    preds_df['target'] = target.astype(float)
+    #fig =plot_box_kde(preds_df, 'target', 'preds')
+    fig =plot_hist_kde(preds_df, 'target', 'preds')
+
+    
+    # CM: Detection rate
+    fig = evaluate_detection_rate(y_preds=preds, y_true=target, threshold=threshold)
+    
+
+    
+    #plot_net_benefit(target, preds)
+
+    # benefit
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Plot the net benefit curve on the created axis
+    net_benefit_display.plot(ax=ax)
+
+    # Set the X-axis maximum to 0.2
+    ax.set_xlim(0.0,1.0)
+    ax.set_ylim(0.0,0.1)
+
+    # Add labels and title
+    ax.set_xlabel('Threshold')
+    ax.set_ylabel('Net Benefit')
+    ax.set_title('Net Benefit Curve')
+
+    # Add a legend
+    ax.legend()
---
+++ src/train_full_model.py
@@ -0,0 +1,234 @@
+import os
+# set workdir as project root for notebook imports.
+compute_name = 'amil-gpu1'
+os.chdir(f'/mnt/batch/tasks/shared/LS_root/mounts/clusters/{compute_name}/code/Users/andreas.skov.millarch/repos/DTTD')
+
+import warnings
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc
+import numpy as np
+
+from fastai.tabular.all import *#TabularPandas, RandomSplitter, CrossEntropyLossFlat, tabular_learner
+from src.utils import find_columns_with_word, clear_mem
+
+import pandas as pd 
+import numpy as np 
+import pickle
+import copy 
+
+from tsai.data.core import get_ts_dls
+from tsai.data.preprocessing import TSStandardize, TSNormalize, TSMultiLabelClassification
+from tsai.data.tabular import get_tabular_dls
+from tsai.data.mixed import get_mixed_dls
+from tsai.data.validation import get_splits
+from tsai.data.preparation import df2xy
+from tsai.all import TensorMultiCategory
+
+from fastai.data.transforms import Categorize
+from fastai.tabular.core import Categorify, FillMissing, Normalize
+from fastai.callback.core import TrainEvalCallback
+
+
+from omegaconf import OmegaConf
+from src.visualize import plot_evaluation, plot_box_kde, plot_loss, plot_fold_evaluation, create_calibration_plot, evaluate_detection_rate
+from sklearn.metrics import auc, roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
+
+from src.data.utils import get_cfg
+from src.training.utils import initialize_metrics, evaluate_and_log_metrics
+from src.training.utils import initialize_fmetrics, evaluate_and_log_fmetrics, calculate_fmetrics_stats 
+from src.common.log_config import setup_logging, clear_log
+from src.evaluation import calculate_roc_auc_ci, evaluate, calculate_average_precision_ci
+from src.dataset.timeseries import TSDS
+from src.data.utils import align_dataframes
+from src.visualize import plot_evaluation, plot_box_kde
+from tsai.all import computer_setup, TSTPlus, count_parameters, TSTabFusionTransformer#,SaveModel
+from tsai.all import get_tabular_dls
+
+from src.training.tsai_custom import CSaveModel
+from src.evaluation import get_internal_validation_thresholds, dl_file_from_run, is_float_in_range
+
+import logging 
+
+setup_logging()
+clear_log()
+logger = logging.getLogger(__name__)
+
+cfg = get_cfg()
+
+# Create a custom callback to skip validation
+class SkipValidationCallback(TrainEvalCallback):
+    def before_validate(self): 
+        raise CancelValidException()
+
+def plot_loss(learn):
+    skip_start = 0
+    plt.plot(list(range(skip_start, len(learn.recorder.losses))), learn.recorder.losses[skip_start:], label='train')
+    idx = (np.array(learn.recorder.iters)<skip_start).sum()
+    plt.legend()
+    plt.savefig('models/loss.png', dpi=1200)    
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load data
+    concepts= ['VitaleVaerdier', 'Labsvar', 'Medicin','ITAOversigtsrapport']#, 'Procedurer']
+    #concepts= ['VitaleVaerdier']
+    trainval = TSDS(cfg, filename='trainval_tab_simple', concepts=concepts)
+    holdout = TSDS(cfg, filename='holdout_tab_simple', concepts=concepts)
+
+    trainval.complete = pd.concat(trainval.concepts).fillna(0.0)
+    holdout.complete = pd.concat(holdout.concepts).fillna(0.0)
+    trainval.complete, holdout.complete = align_dataframes(trainval.complete, holdout.complete)
+
+    # Setup
+    exclude = ['deceased_30d']
+
+    #cat_cols = ['SEX']
+    #cat_cols=cfg["dataset"]["cat_cols"]
+    cat_cols = ['SEX', 'LVL1TC']
+    #ppj_cat_cols = cfg["dataset"]["ppj_cat_cols"]
+    ppj_cat_cols = ["A","B","C","D"]
+
+    cat_cols = cat_cols+ppj_cat_cols
+    num_cols = [i for i in trainval.tab_df.columns if i not in cat_cols and i != cfg["target"] and i not in exclude]
+    logger.info(f'Categoricals: {cat_cols}\nNumericals: {num_cols}')
+
+    # HOLDOUT 
+
+    # Note, no batch shuffle on dataloder
+    logger.info('Preparing holdout dataloader')
+    tfms = [None, [Categorize()]]
+    batch_tfms = TSStandardize(by_var=True)
+    procs = [Categorify, FillMissing, Normalize]
+    # Holdout set
+    tX, ty = df2xy(holdout.complete, 
+                sample_col='PID', 
+                feat_col='FEATURE', 
+                data_cols=holdout.complete.columns[3:], 
+                target_col=holdout.target)
+    ty = ty[:, 0].flatten()
+    ty = list(ty)
+    logger.info(f'Holdout X shape: {tX.shape}')
+    test_ts_dls = get_ts_dls(tX, ty, splits=None, tfms=tfms, batch_tfms=batch_tfms, bs=cfg["training"]["bs"], drop_last=False,  shuffle=False)
+
+    test_tab_dls = get_tabular_dls(holdout.base, procs=procs, 
+                                cat_names=cat_cols.copy(), 
+                                cont_names=num_cols.copy(), 
+                                y_names=cfg["target"], 
+                                splits=None, 
+                                drop_last=False,
+                                shuffle=False)
+
+
+    holdout_mixed_dls = get_mixed_dls(test_ts_dls, test_tab_dls, bs=cfg["training"]["bs"], shuffle_valid=False)
+
+    # FOR CLASSES!
+    complete_tab_dls = get_tabular_dls(pd.concat([trainval.base, holdout.base]), procs=procs, 
+                            cat_names=cat_cols.copy(), 
+                            cont_names=num_cols.copy(), 
+                            y_names=cfg["target"], 
+                            splits=None, 
+                            drop_last=False,
+                            shuffle=False)
+    classes = complete_tab_dls.classes
+
+    # Training and validation dataset
+    logger.info("Setting up X,y for training and validation")
+    X, y = df2xy(trainval.complete, 
+            sample_col='PID', 
+            feat_col='FEATURE', 
+            data_cols=trainval.complete.columns[3:], 
+            target_col=cfg["target"])
+    y = y[:, 0].flatten()
+    y = list(y)
+    logger.info(f'Train/val X shape: {X.shape}')
+
+    # TRAINING
+    logger.info('Preparing training cycle')
+    # Initialize vars to store results
+    metrics = initialize_metrics()
+    fmetrics = initialize_fmetrics()
+
+    ts_dls = get_ts_dls(X, y, splits=None, tfms=tfms, batch_tfms=batch_tfms, bs=cfg["training"]["bs"], drop_last=False)
+
+    tab_dls = get_tabular_dls(trainval.tab_df, procs=procs, 
+                            cat_names=cat_cols.copy(), 
+                            cont_names=num_cols.copy(), 
+                            y_names=cfg["target"], 
+                            splits=None, bs=cfg["training"]["bs"],
+                            drop_last=False)
+
+    mixed_dls = get_mixed_dls(ts_dls, tab_dls, 
+                            bs=cfg["training"]["bs"])
+    
+    model = TSTabFusionTransformer(c_in = mixed_dls.vars, c_out = 2,
+                                      seq_len =  mixed_dls.len, classes=classes, cont_names= num_cols, 
+                                       n_layers = 8,
+                                       n_heads = 8,
+                                       d_model=64,
+                                       fc_dropout=0.75, 
+                                       res_dropout=0.22,
+                                      fc_mults = (0.3, 0.1),
+                              )
+
+    logger.debug(f'numcols: {num_cols}')
+
+    loss_func = LabelSmoothingCrossEntropyFlat()
+
+    learn = Learner(mixed_dls, model,
+                    loss_func=loss_func,
+                    metrics=None,
+                    cbs=[SkipValidationCallback()]
+                )
+    logger.debug(mixed_dls.vars, 2, f"seq_len={mixed_dls.len}",f"n_cat ={len(classes.keys())}", f"n_cont={len(num_cols)}" )
+
+    lr = 0.0004786300996784121
+
+    learn.fit_one_cycle(20, lr)    
+    plot_loss(learn)
+
+    learn.save('full_model')
+    # Load saved model into learner for evaluation
+    # Necessary due to the modified training proces without validation set before
+    clear_mem()
+
+    model = TSTabFusionTransformer(mixed_dls.vars, 2,
+                                       mixed_dls.len, classes, num_cols, 
+                                       n_layers = 8,
+                                       n_heads = 8,
+                                       d_model=64,
+                                       fc_dropout=0.75, 
+                                       res_dropout=0.22,
+                                      fc_mults = (0.3, 0.1),
+                              )
+
+    logger.debug(f'numcols: {num_cols}')
+
+    loss_func = LabelSmoothingCrossEntropyFlat()
+
+    learn = Learner(mixed_dls, model,
+                    loss_func=loss_func,
+                    metrics=None,
+                )
+    learn = learn.load( 'full_model')
+
+    beta=5
+    thresholds = get_internal_validation_thresholds(beta=beta)
+    thresholds = [t for t in thresholds if t <0.5]
+    threshold = np.average(thresholds)
+
+    evaluate(holdout_mixed_dls, learn, threshold)
+
+    preds, target = learn.get_preds(dl=holdout_mixed_dls.train)
+    preds = preds[:,1].cpu().numpy()
+    target = target.cpu().numpy()
+
+    auc, auc_ci_lower, auc_ci_upper = calculate_roc_auc_ci(target, preds)
+    ap, ap_ci_lower, ap_ci_upper = calculate_average_precision_ci(target, preds)
+
+    logger.info(f"ROC AUC: {auc:.3f} (95% CI: {auc_ci_lower:.3f}-{auc_ci_upper:.3f})")
+    logger.info(f"Average Precision: {ap:.3f} (95% CI: {ap_ci_lower:.3f}-{ap_ci_upper:.3f})")
+
+if __name__ == '__main__':
+    main()
